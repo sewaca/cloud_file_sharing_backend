b8f04368c268939c522b4e6a36f001d67fc6ea1e
sewaca <sewaca7@gmail.com>
2024-04-13 23:09:32 +0300

    отсортировал хедеры

--- server/headers.php
+++ server/headers.php
@@ -1,10 +1,9 @@
 <?php
 
 // Response headers : 
-header("Access-Control-Allow-Origin: *");
-header('Content-Type: application/json; charset=UTF-8');
-header('Access-Control-Allow-Methods: "GET,POST,OPTIONS,DELETE,PUT"');
-header("Access-Control-Max-Age: 360000");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Methods: "GET,POST,OPTIONS,DELETE,PUT"');
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Max-Age: 360000");
+header('Content-Type: application/json; charset=UTF-8');
