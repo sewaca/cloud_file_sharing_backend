5daefc332bd46507cd9b1d7695dbd984318a182c
sewaca <sewaca7@gmail.com>
2024-04-25 21:55:20 +0300

    fixed logger error

--- server/index.php
+++ server/index.php
@@ -1,5 +1,11 @@
 <?php 
 
+if ($_ENV['IS_BUILD']) {
+    error_reporting(E_ERROR);
+} else {
+    error_reporting(E_ALL);
+}
+
 define('BASE_PATH', ($_ENV['IS_BUILD'] ? '' : '.'));
 define('TEMP_PATH', ($_ENV['IS_BUILD'] ? sys_get_temp_dir() : BASE_PATH."/server/temp"));
 
